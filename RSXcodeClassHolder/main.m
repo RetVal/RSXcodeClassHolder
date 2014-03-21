1f3c1c1e979056c22d8fa741e445d2193fbb4b47
closure <retval@me.com>
2014-03-21 16:40:44 +0800

    Initial Commit

---
+++ RSXcodeClassHolder/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RSXcodeClassHolder
+//
+//  Created by closure on 3/21/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
